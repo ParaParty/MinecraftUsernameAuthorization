f896d2578736fea848cf9cd5cf77407a4e6d5a19
Eric_Lian <git@superexercisebook.com>
2022-01-08 14:44:53 +0800

    feat: forge 版本

--- forge-1_16_5/src/main/java/party/para/yggdrasil/forge_1_16_5/ParapartyAuthorizationForge.java
+++ forge-1_16_5/src/main/java/party/para/yggdrasil/forge_1_16_5/ParapartyAuthorizationForge.java
@@ -1,85 +1,68 @@
 package party.para.yggdrasil.forge_1_16_5;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import party.para.yggdrasil.authorization.core.model.CheckResult;
+import party.para.yggdrasil.authorization.core.service.AuthorizationService;
+import party.para.yggdrasil.authorization.core.service.implementation.AbstractAuthorizationService;
 
-import java.util.stream.Collectors;
+import java.util.Locale;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("paraparty-authorization-forge")
+@Mod("paraparty-authorization")
 public class ParapartyAuthorizationForge {
 
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ParapartyAuthorizationForge() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    AuthorizationService authService = new AbstractAuthorizationService() {
+        @Override
+        public void onFail() {
 
-        // Register ourselves for server and other game events we are interested in
+        }
+
+        @Override
+        public void onError(Exception ex) {
+            LOGGER.error(ex.getMessage(), ex);
+        }
+
+        @Override
+        public void onSuccess(CheckResult ret) {
+
+        }
+    };
+
+    public ParapartyAuthorizationForge() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void playerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        PlayerEntity playerObj = event.getPlayer();
+        if (!(playerObj instanceof ServerPlayerEntity)) {
+            return;
+        }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
+        ServerPlayerEntity player = (ServerPlayerEntity) playerObj;
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("paraparty-authorization-forge", "helloworld", () -> {
-            LOGGER.info("Hello world from the MDK");
-            return "Hello world";
-        });
-    }
+        Locale locale = Locale.getDefault();
+        String name = player.getName().getString();
 
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-            map(m -> m.getMessageSupplier().get()).
-            collect(Collectors.toList()));
-    }
+        CheckResult result = authService.checkStatus(name, locale == null ? "zh_cn" : locale.toString());
+        if (result == null) {
+            player.connection.disconnect(new StringTextComponent(""));
+            return;
+        }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
+        if (result.getStatus() != 0) {
+            player.connection.disconnect(new StringTextComponent(result.getMessage()));
+            return;
         }
     }
 }
